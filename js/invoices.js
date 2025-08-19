e746831f3e22e090fa1f0dc55d9f6b07713974d7
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-20 00:16:55 +0430

    create Function to add a new invoice

--- js/invoices.js
+++ js/invoices.js
@@ -17,3 +17,37 @@ const clientSelect = document.getElementById("invoice-client");
 function renderInvoices() {
   renderTableRows(invoices, invoiceList, createInvoiceRow);
 }
+
+invoiceForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const clientId = clientSelect.value;
+  const serviceTitle = document.getElementById("invoice-service").value.trim();
+  const description = document
+    .getElementById("invoice-description")
+    .value.trim();
+  const amount = parseFloat(
+    document.getElementById("invoice-amount").value.trim()
+  );
+  const date = document.getElementById("invoice-date").value.trim();
+
+  const client = clients.find((c) => c.id === parseInt(clientId));
+
+  const newInvoice = {
+    id: generateUniqueId(),
+    client,
+    serviceTitle,
+    description,
+    amount,
+    date,
+    paid: false,
+  };
+
+  invoices.push(newInvoice);
+
+  saveToLocalStorage("invoices", invoices);
+
+  invoiceForm.reset();
+
+  renderInvoices();
+});
