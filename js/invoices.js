1e643c6641a0c41ebe990bb978255a1ec35bcbc6
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-20 00:14:15 +0430

    Initialize invoice form, list, and client select elements

--- js/invoices.js
+++ js/invoices.js
@@ -7,3 +7,9 @@ import {
 
 let invoices = JSON.parse(localStorage.getItem("invoices")) || [];
 let clients = JSON.parse(localStorage.getItem("clients")) || [];
+
+const invoiceForm = document.getElementById("invoice-form");
+const invoiceList = document
+  .getElementById("invoice-list")
+  .getElementsByTagName("tbody")[0];
+const clientSelect = document.getElementById("invoice-client");
