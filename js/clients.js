d70982d0eee8f97566c00cb604ab7c0ff6299636
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-19 17:37:01 +0430

    Initialize clients from localStorage and render client list

--- js/clients.js
+++ js/clients.js
@@ -4,3 +4,14 @@ import {
   createClientRow,
   generateUniqueId,
 } from "./utils.js";
+
+let clients = JSON.parse(localStorage.getItem("clients")) || [];
+
+const clientForm = document.getElementById("client-form");
+const clientList = document
+  .getElementById("client-list")
+  .getElementsByTagName("tbody")[0];
+
+function renderClients() {
+  renderTableRows(clients, clientList, createClientRow);
+}
