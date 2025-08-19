a5c7cb2c7fef909a224e28988b882247cd0a76fd
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-19 17:25:31 +0430

    define renderTableRows for rendering rows

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,12 @@
 function saveToLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+function renderTableRows(data, tbodyElement, renderRow) {
+  tbodyElement.innerHTML = "";
+
+  data.forEach((item) => {
+    const row = renderRow(item);
+    tbodyElement.appendChild(row);
+  });
+}
