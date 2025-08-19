e47b5fa98ba645287d0fc210df5daccb28634abd
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-19 17:26:17 +0430

    create createClientRow function for creating client

--- js/utils.js
+++ js/utils.js
@@ -10,3 +10,25 @@ function renderTableRows(data, tbodyElement, renderRow) {
     tbodyElement.appendChild(row);
   });
 }
+
+function createClientRow(client) {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+
+    <td>${client.name}</td>
+    <td>${client.email}</td>
+    <td>${client.company}</td>
+    <td>${client.notes || "N/A"}</td>
+    <td>
+      <button class="btn btn-edit" onclick="editClient(${
+        client.id
+      })">  <i class="bi bi-pen text-success"></i></button>
+      <button class="btn btn-delete" onclick="deleteClient(${
+        client.id
+      })"><i class="bi bi-x-circle text-danger"></i></button>
+    </td>
+  `;
+
+  return row;
+}
