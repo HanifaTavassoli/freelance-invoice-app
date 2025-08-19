c005afe00076fd2cc76089d321e446c4ab32e589
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-20 00:12:43 +0430

    Retrieve stored invoices and clients from localStorage

--- js/invoices.js
+++ js/invoices.js
@@ -4,3 +4,6 @@ import {
   createInvoiceRow,
   generateUniqueId,
 } from "./utils.js";
+
+let invoices = JSON.parse(localStorage.getItem("invoices")) || [];
+let clients = JSON.parse(localStorage.getItem("clients")) || [];
