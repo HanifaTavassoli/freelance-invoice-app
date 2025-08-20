7f34ae86c76c726986ed163eca6e3f9864619792
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-20 10:39:56 +0430

    Implement summary update functionality

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ const paidCount = document.getElementById("paid-count");
 const unpaidCount = document.getElementById("unpaid-count");
 const quoteText = document.getElementById("quote-text");
 const quoteAuthor = document.getElementById("quote-author");
+
+function updateSummary() {
+  console.log("client");
+  clientCount.textContent = clients.length;
+  invoiceCount.textContent = invoices.length;
+
+  const totalInvoiceValue = invoices.reduce(
+    (total, invoice) => total + invoice.amount,
+    0
+  );
+  totalValue.textContent = totalInvoiceValue.toFixed(2);
+
+  const paidInvoices = invoices.filter((invoice) => invoice.paid);
+  const unpaidInvoices = invoices.filter((invoice) => !invoice.paid);
+
+  paidCount.textContent = paidInvoices.length;
+  // unpaidCount.textContent = unpaidInvoices.length;
+}
